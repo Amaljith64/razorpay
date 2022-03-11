e54249819aecbc872f84bf0cf0c53acf4ba87682
Amaljith <amaljith64@gmail.com>
2022-03-11 10:46:44 +0530

    project done

--- src/views.py
+++ src/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+import razorpay
+
+from .models import Coffee
+
+from django.views.decorators.csrf import csrf_exempt
+
 
 def home(request):
-    return render(request,'index.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        amount = int(request.POST.get("amount")) * 100
+        client = razorpay.Client(auth=("rzp_test_ZY4ulZfKRFRCrI", "7ExtOPzLE2HVDB8cJdcTnFYV"))
+        payment = client.order.create({'amount': amount, 'currency': 'INR', 'payment_capture': '1'})
+        print(payment)
+        coffee = Coffee(name=name, amount=amount, payment_id=payment['id'])
+        coffee.save()
+        return render(request, 'index.html', {'payment': payment})
+
+    return render(request, 'index.html')
 
 
+@csrf_exempt
 def success(request):
-    return render(request,'success.html')
+    if request.method == "POST":
+        a = request.POST
+        order_id = ""
+        for key, val in a.items():
+            if key == 'razorpay_order_id':
+                order_id = val
+                break
+        user = Coffee.objects.filter(payment_id=order_id).first()
+        user.paid = True
+        user.save()
+    return render(request, 'success.html')
